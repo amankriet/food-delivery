d76ab98b662df7c91bb3bbe206158cb18abb1e64
amankriet <amankriet@gmail.com>
2024-05-11 11:53:34 +0530

    build done

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
+import vitePluginSvgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    // build: {
+    //     transpileDependencies: ['fsevents']
+    // },
+    optimizeDeps: {
+        esbuildOptions: {
+            jsx: "automatic",
+        }
+    },
+    plugins: [
+        react(),
+        vitePluginSvgr()
+    ],
 })
