2f8274d7837f0387e1833982df406cea0ca919f5
amankriet <amankriet@gmail.com>
2024-06-06 07:38:01 +0530

    added shimmer ui for restaurants

--- src/components/RestaurantCard.jsx
+++ src/components/RestaurantCard.jsx
@@ -3,21 +3,26 @@ import {imgBaseUrl} from "../utils/constants.js"
 
 function RestaurantCard(props) {
     const {
-        aggregatedDiscountInfoV3,
         name,
         area,
         cloudinaryImageId,
         cuisines,
         costForTwo,
         sla,
+        slaString,
         promoted,
         avgRating
-    } = props?.restaurant?.info
+    } = props?.restaurant?.info || props.restaurant?.data
+    
     let header = '', subHeader = ''
-    if (aggregatedDiscountInfoV3) {
+    if (props?.restaurant?.info?.aggregatedDiscountInfoV3) {
         header = aggregatedDiscountInfoV3.header
         subHeader = aggregatedDiscountInfoV3.subHeader
         // console.log('header:', header, 'subHeader:', subHeader, ' info:', aggregatedDiscountInfoV3)
+    } else {
+        let aggregatedDiscountInfo = props?.restaurant?.data?.aggregatedDiscountInfo
+        header = aggregatedDiscountInfo.header
+        subHeader = aggregatedDiscountInfo.subHeader
     }
 
     return (
@@ -25,7 +30,7 @@ function RestaurantCard(props) {
             <ErrorBoundary fallback={<h2>Error while loading restaurant image</h2>}>
                 <div className="restaurant-img">
                     <img src={imgBaseUrl + cloudinaryImageId} alt={name}/>
-                    {aggregatedDiscountInfoV3 && (
+                    {header  && (
                         <h2 className='discount-header'>{`${header} ${subHeader}`}</h2>
                     )}
                 </div>
@@ -35,7 +40,7 @@ function RestaurantCard(props) {
                     <p className={'restaurant-name'}>{name}</p>
                     <p className="meta">
                         <span className="star">Rating</span> <span
-                        className="meta-text">{avgRating} &middot; {sla?.slaString}</span>
+                        className="meta-text">{avgRating} &middot; {sla?.slaString || slaString}</span>
                     </p>
                 </div>
                 <div className="sub-text">
--- src/components/Restaurants.jsx
+++ src/components/Restaurants.jsx
@@ -1,20 +1,35 @@
 import RestaurantCard from "./RestaurantCard.jsx"
-import ErrorBoundary from "./ErrorBoundary.jsx";
+import ErrorBoundary from "./ErrorBoundary.jsx"
+import ShimmerRestaurants from './ShimmerRestaurants.jsx'
 
 function Restaurants({ restaurants }) {
+    const restaurantsArray = []
+
+    const getRestaurantCards = (count) => {
+        for (var i = 0; i <= count; i++) {
+            restaurantsArray.push(<ShimmerRestaurants key={i} />)
+        }
+
+        return restaurantsArray
+    }
+
     return (
         <main>
             <ErrorBoundary>
                 {/*<h2 className={'data-missing'}>No restaurants available</h2>*/}
                 <div className="grid-container">
-                    {restaurants && restaurants.length > 0 && (
-                        restaurants.map((restaurant, index) => (
-                            <RestaurantCard key={index} restaurant={restaurant}/>))
-                    )}
-                </div>
-            </ErrorBoundary>
-        </main>
-    )
+                    {
+                        (restaurants && restaurants.length > 0) ? (
+                            restaurants.map((restaurant, index) => (
+                                <RestaurantCard key={index} restaurant={restaurant}/>))
+                            ) 
+                        : (getRestaurantCards(16)
+                            )
+                    }
+                    </div>
+                    </ErrorBoundary>
+                    </main>
+                    )
 }
 
 export default Restaurants
